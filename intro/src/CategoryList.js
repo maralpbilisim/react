3e941f1b5f24011a5cefe4430dcb5348e2ffe5be
maralpbilisim <72076365+maralpbilisim@users.noreply.github.com>
2022-10-08 17:10:23 +0300

    state  example

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react'
-import { ListGroup,ListGroupItem} from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 export default class CategoryList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories: [
+                { categoryId: 1, categoryName: "içecek" },
+                { categoryId: 2, categoryName: "Yemek" }
+            ]
+        }
+    }
+
     render() {
         return (
             <div><h3>{this.props.info.title}</h3>
                 <ListGroup>
-                    <ListGroupItem>
-                        Cras justo odio
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Dapibus ac facilisis in
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Morbi leo risus
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Porta ac consectetur ac
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Vestibulum at eros
-                    </ListGroupItem>
+                    {
+                        this.state.categories.map(category => (
+                            <ListGroupItem  key={category.categoryId}>
+                                {category.categoryName}
+                            </ListGroupItem>
+                        ))
+                    }
+
+
                 </ListGroup>
             </div>
         )
