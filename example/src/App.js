44193029d42bdf0318e711f206d7a612341b03a9
maralpbilisim <72076365+maralpbilisim@users.noreply.github.com>
2022-10-21 20:22:24 +0300

    example

---
+++ example/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="mydiv">
+   <h2>Welcome to React!</h2> <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
+    </div>
+  );
+}
+
+export default App;
---
+++ example/src/comp.js
@@ -0,0 +1,14 @@
+import Deneme from "./deneme";
+
+const comp = function() {
+    return (
+      <div>
+        <h2>comp</h2>
+        <ul>
+            <li><Deneme/></li>
+        </ul>
+    </div>
+    );
+  };
+
+export default comp;
